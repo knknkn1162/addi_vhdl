71029ec8869ac3aa2907fda8cf9000260824762b
knknkn1162 <knknkn1162@gmail.com>
2018-12-24 12:37:55 +0900

    polish regfile component

--- src/regfile.vhdl
+++ src/regfile.vhdl
@@ -2,6 +2,7 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
+-- simple_dual_port_ram_single_clock
 entity regfile is
   generic(ADDR_WIDTH: natural);
   port (
@@ -15,32 +16,16 @@ end entity;
 architecture behavior of regfile is
   type ram_type is array(natural range<>) of std_logic_vector(31 downto 0);
 
-  -- function init_ram return ram_type is
-  --   variable tmp : ram_type(0 to 2**ADDR_WIDTH-1) := (others => (others => '0'));
-  -- begin
-  --   for addr_pos in 0 to 2**ADDR_WIDTH - 1 loop
-  --     -- Initialize each address with the address itself
-  --     tmp(addr_pos) := std_logic_vector(to_unsigned(addr_pos, 32));
-  --   end loop;
-  --   return tmp;
-  -- end function;
-
-  -- Declare the RAM signal and specify a default value. Quartus Prime
-  -- will create a memory initialization file (.mif) based on the 
-  -- default value.
   signal s_ram : ram_type(0 to 2**ADDR_WIDTH-1) := (others => (others => '0'));
 begin
   process(clk, i_we, i_wa, i_wd)
-    variable idx : natural range 0 to 2**ADDR_WIDTH-1 := 0;
   begin
     if rising_edge(clk) then
       if i_we = '1' then
-        idx := to_integer(unsigned(i_wa));
-        if idx /= 0 then
-          s_ram(idx) <= i_wd;
-        end if;
+        s_ram(to_integer(unsigned(i_wa))) <= i_wd;
       end if;
     end if;
   end process;
-  o_rd <= s_ram(to_integer(unsigned(i_ra)));
+  -- register 0 hardwired to 0
+  o_rd <= X"00000000" when to_integer(unsigned(i_ra)) = 0 else s_ram(to_integer(unsigned(i_ra)));
 end architecture;
